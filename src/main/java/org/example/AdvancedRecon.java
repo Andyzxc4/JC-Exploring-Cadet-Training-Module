bd44b0e6a23831c01cc624c0dd525a99c0142e8c
Andyzxc4 <lacraandre111@gmail.com>
2025-08-14 10:11:57 +0800

    feat: add final project structure & output screenshots

---
+++ src/main/java/org/example/AdvancedRecon.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// this interface extends two other interfaces (VisualRecon & SignalIntel)
+public interface AdvancedRecon extends VisualRecon, SignalIntel {
+    // This interface is a combination of two contracts.
+    // It doesn't need a body, it just aggregates other interfaces.
+}
+
+
+
+
